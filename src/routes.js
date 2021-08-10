b03ceee3f1e532048797421a3de72f41bd1d9164
BomDia12 <chamatico2@gmail.com>
2021-08-10 16:46:07 -0300

    :construction: Removing changes to routes
    removing changes to routes, to enable merge

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import { Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
-import CreatePainter from './pages/CreatePainter'
 
 export const Routes = () => {
     return (
@@ -10,7 +9,6 @@ export const Routes = () => {
             <Route exact path='/' component={Home} />
             <Route path='/login' component={LoginPage} />
             <Route path='/register' component={RegisterPage} />
-            <Route path='/painter/create' component={CreatePainter}/>
         </Switch>
     )
 }
