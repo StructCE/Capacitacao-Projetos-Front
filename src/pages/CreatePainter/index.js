3171d5afbe63c95d9cf175d3819850be53b75620
Draculk <kayranvieira456@hotmail.com>
2021-08-10 16:36:16 -0300

    :construction: campos e visual

--- src/pages/CreatePainter/index.js
+++ src/pages/CreatePainter/index.js
@@ -1,11 +1,55 @@
-import Input from "../../components/Input"
-import {FiCalendar, FiUser, FiBookOpen} from 'react-icons/fi'
-import {GiPirateGrave} from 'react-icons/gi'
-import TextArea from "../../components/TextArea"
-import Button from "../../components/Button"
-import { LeftSector, RightSector, CenterSector, Container } from "../CreatePainter/style"
+import Input from "../../components/Input";
+import { FiCalendar, FiUser, FiBookOpen } from "react-icons/fi";
+import { GiPirateGrave } from "react-icons/gi";
+import TextArea from "../../components/TextArea";
+import Button from "../../components/Button";
+import {
+  LeftSector,
+  RightSector,
+  CenterSector,
+  Container,
+  InputSection
+} from "../CreatePainter/style";
+import { api } from "../../services/api";
+import { useState } from "react";
+import { useHistory } from 'react-router-dom'
 
 const CreatePainter = () => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const [died, setDied] = useState("");
+  const [bio, setBio] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (name === "") {
+      alert("Nome deve estar preenchido.");
+    } else if (!born) {
+      console.log(born)
+      alert("Data de nascimento deve estar preenchida.");
+    } else if (died === "") {
+      alert("Data de óbito deve estar preenchida.");
+    } else if (bio === "") {
+      alert("Biografia de senha deve estar preenchida.");
+    }
+
+    const response = await api.post('/painter/create', {
+      painter: {
+        name, 
+        born, 
+        died, 
+        bio
+      }
+    })
+
+    if(response.data){
+      alert('pintor criado!')
+      history.push('/login')
+    }
+  };
+
   return (
     <Container>
       <LeftSector>
@@ -13,38 +57,35 @@ const CreatePainter = () => {
         <div className="gradient" />
       </LeftSector>
       <CenterSector>
-      <h1>Adicione novos pintores</h1>
-      <Input
-        Icon={FiUser}
-        placeholder={"Nome do Artista"}
-        type={"text"}
-      />
+        <InputSection onSubmit={handleSubmit}>
+        <h1>Adicione novos pintores</h1>
+        <Input Icon={FiUser} placeholder={"Nome do Artista"} type={"text"} onChange={(value) => setName(value.target.value)} />
 
-      <Input 
-        Icon={FiCalendar}
-        placeholder={"Data de Nascimento"}
-        type={"date"}
-      />
+        <Input
+          Icon={FiCalendar}
+          placeholder={"Data de Nascimento"}
+          type={"date"}
+          onChange={(value) => setBorn(value.target.value)}
+        />
 
-      <Input
-        Icon={GiPirateGrave}
-        placeholder={"Data de Morte"}
-        type={"date"}
-      />
+        <Input
+          Icon={GiPirateGrave}
+          placeholder={"Data de Morte"}
+          type={"date"}
+          onChange={(value) => setDied(value.target.value)}
+        />
 
-      <TextArea
-        Icon={FiBookOpen}
-        placeholder={"Biografia"}
-      />
+        <TextArea Icon={FiBookOpen} placeholder={"Biografia"} onChange={(value) => setBio(value.target.value)}/>
 
-      <Button>Criar Pintor</Button>
+        <Button type="submit">Criar Pintor</Button>
+        </InputSection>
       </CenterSector>
       <RightSector>
         <div className="gradient" />
         <div className="transparent" />
       </RightSector>
     </Container>
-  )
-}
+  );
+};
 
-export default CreatePainter
+export default CreatePainter;
