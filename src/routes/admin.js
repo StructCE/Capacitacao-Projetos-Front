f45f7a1837a29614e81187430202aed1f6459f4f
Iago Carvalho Cavalcante <carvalhoiago@live.com>
2021-08-16 23:33:05 -0300

    ✨ Create e Update estilos funcionando, para usar o update tem que passar o id pela URL

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,6 +2,7 @@ import { Route, Switch, useRouteMatch } from "react-router-dom"
 import { useUserContext } from "../hooks/useUserContext"
 import { Home } from "../pages/Home"
 import CreateStyle from "../pages/Style/CreateStyle"
+import UpdateStyle from "../pages/Style/UpdateStyle"
 
 const AdminRoutes = () => {
     
@@ -17,6 +18,7 @@ const AdminRoutes = () => {
                     user.is_admin ?
                     <>
                         <Route exact path={`${path}/create-style`} component={CreateStyle}/>
+                        <Route exact path={`${path}/update-style/:id`} component={UpdateStyle}/>
                         <Route exact path={path} />
                     </>
                     :
